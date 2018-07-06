ed33ed4c66cff11681dab3226ae29c558f5dde3d
StavrosDidakis <stavrosdee@msn.com>
2018-07-07 01:20:03 +0800

    changes

--- 03_Variation/03_Modulo_And_Rotations1-(checker)/sketch.js
+++ 03_Variation/03_Modulo_And_Rotations1-(checker)/sketch.js
@@ -3,12 +3,10 @@
 //OFCourse, Thursday 5 July 2018 - Sunday 8 July 2018
 //Shanghai, China
 
-//03_Modulo and Rotations
+//03_Modulo and Rotations 1 (checker)
 
 //Set variables
 let size = 50;
-let angle = 0;
-let jitter = 0;
 
 function setup() {
   createCanvas(500, 500);
@@ -16,11 +14,6 @@ function setup() {
 }
 
 function draw() {
-  //Create an automated rotation of all middle shapes
-  if (second() % 2 == 0) jitter = random(-0.1, 0.1);
-  angle = angle + jitter;
-  let c = cos(angle); //variable c defines the final rotation
-
   translate(25, 25);  //Offset all shapes by half their width and height
 
   //Run the nested loop
@@ -41,13 +34,11 @@ function draw() {
       } else if (x%2==1 && y%2==0) {
         fill(200, 200, 50);
       } else fill(50, 200, 0);
-      //console.log(x%2); //see in the console the results from x%2
 
       //To keep our transformations correctly arranged,
       //it is necessary to have a push/pop translation system
       push(); //apply this at the beginning of the transformation
       translate(size * x, size * y); //assign here the shape position
-      rotate(c); //rotate takes the number from the automated rotation
       rect(0, 0, size/2, size/2); //position is driven by translate()
       pop(); //apply this at the end of the transformation
     }
